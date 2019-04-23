43401130fcfecfd0749f0c286e4c63a823531080
dhanyn10 <dhanyn.dhan@gmail.com>
2019-04-23 17:12:38 +0700

    membuat UI dengan Java, improv

--- androidtest2javaUI/app/src/main/java/blank/android/test/androidtest2_javaui/MainActivity.java
+++ androidtest2javaUI/app/src/main/java/blank/android/test/androidtest2_javaui/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.RelativeLayout;
 import android.widget.Button;
+import android.graphics.Color;
 
 public class MainActivity extends Activity {
 
@@ -12,9 +13,23 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         RelativeLayout rl = new RelativeLayout(this);
-        Button btn = new Button(this);
+        rl.setBackgroundColor(Color.BLUE);
 
-        rl.addView(btn);
+        Button btn = new Button(this);
+        btn.setBackgroundColor(Color.BLACK);
+        btn.setText("Submit");
+        btn.setTextColor(Color.WHITE);
+
+        RelativeLayout.LayoutParams btndetail =
+        new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT, //for width
+                RelativeLayout.LayoutParams.WRAP_CONTENT //for height
+        );
+
+        btndetail.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        btndetail.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        rl.addView(btn, btndetail);
         setContentView(rl);
     }
 }
