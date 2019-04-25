56ab294ba02d52f7b9c462a757ede77a98346410
dhanyn10 <dhanyn.dhan@gmail.com>
2019-04-25 11:47:29 +0700

    update dengan ujicoba menggunakan component setminwidth

--- androidtest2javaUI/app/src/main/java/blank/android/test/androidtest2_javaui/MainActivity.java
+++ androidtest2javaUI/app/src/main/java/blank/android/test/androidtest2_javaui/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.widget.RelativeLayout;
 import android.widget.Button;
 import android.graphics.Color;
-
+import android.widget.EditText;
 public class MainActivity extends Activity {
 
     @Override
@@ -16,20 +16,33 @@ public class MainActivity extends Activity {
         rl.setBackgroundColor(Color.BLUE);
 
         Button btn = new Button(this);
+        btn.setId(1);
         btn.setBackgroundColor(Color.BLACK);
         btn.setText("Submit");
         btn.setTextColor(Color.WHITE);
 
-        RelativeLayout.LayoutParams btndetail =
-        new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT, //for width
-                RelativeLayout.LayoutParams.WRAP_CONTENT //for height
-        );
+        EditText et1 = new EditText(this);
+        et1.setId(2);
+        et1.setMinWidth(200);
+        RelativeLayout.LayoutParams wrapper1 =
+            new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT, //for width
+                    RelativeLayout.LayoutParams.WRAP_CONTENT //for height
+            );
 
-        btndetail.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        btndetail.addRule(RelativeLayout.CENTER_VERTICAL);
+        RelativeLayout.LayoutParams wrapper2 =
+            new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+            );
 
-        rl.addView(btn, btndetail);
+        wrapper1.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        wrapper1.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        wrapper2.addRule(RelativeLayout.ABOVE, btn.getId());
+        wrapper2.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        rl.addView(btn, wrapper1);
+        rl.addView(et1, wrapper2);
         setContentView(rl);
     }
 }
