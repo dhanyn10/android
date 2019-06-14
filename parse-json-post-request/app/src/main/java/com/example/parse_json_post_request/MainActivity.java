1d99b26dda9427afe528d002b8f11497b092ff98
dhanyn10 <dhanyn.dhan@gmail.com>
2019-06-14 09:02:43 +0700

    update

--- parse-json-post-request/app/src/main/java/com/example/parse_json_post_request/MainActivity.java
+++ parse-json-post-request/app/src/main/java/com/example/parse_json_post_request/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.parse_json_post_request;
 
-import android.provider.SyncStateContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,49 +7,45 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mtext;
+    private TextView ntext, ptext;
     private RequestQueue mRqueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mtext = findViewById(R.id.text_view_result);
-        Button btnparse = findViewById(R.id.btn_parse);
+        ntext       = findViewById(R.id.text_name);
+        ptext       = findViewById(R.id.text_price);
+        Button btnsubmit = findViewById(R.id.btn_submit);
         mRqueue = Volley.newRequestQueue(this);
 
-        btnparse.setOnClickListener(new View.OnClickListener() {
+        btnsubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                jsonParse();
+                jsonInput();
             }
         });
     }
-    private void jsonParse()
+    private void jsonInput()
     {
         JSONArray jsonArray = new JSONArray();
         JSONObject jsonObject = new JSONObject();
         try{
-            jsonObject.put("token", "mOgHIFaj");
-            jsonObject.put("name", "android");
-            jsonObject.put("price", 3000);
+            jsonObject.put("token", getResources().getString(R.string.token));
+            jsonObject.put("name", ntext.getText().toString());
+            jsonObject.put("price", Integer.parseInt(ptext.getText().toString()));
             jsonArray.put(jsonObject);
         }
         catch (Exception err)
@@ -62,12 +57,12 @@ public class MainActivity extends AppCompatActivity {
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        Log.d("RESPONSE: ", response.toString());
+                        Log.d("RESPONSE ", response.toString());
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.d("ERROR: ", error.toString());
+                Log.d("ERROR ", error.toString());
             }
         });
         mRqueue.add(jsonreq);
